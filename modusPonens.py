fa098dcc4b68834f8265992a3c3da13a8359a26e
Juan Cuevas <78194459+jcuevas340@users.noreply.github.com>
2023-08-28 19:32:07 -0600

    Add files via upload

---
+++ modusPonens.py
@@ -0,0 +1,15 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# Afirmaciones
+afirmacion1 = True # Si esta prendida la estufa, el sarten estara caliente
+afirmacion2 = True # La estufa esta prendida
+
+# Modus Ponens
+if afirmacion2:
+    if afirmacion1:
+        conclusion = "El sarten esta caliente"
+        print(conclusion)
+
+        
+El sarten esta caliente
+# Afirmaciones
